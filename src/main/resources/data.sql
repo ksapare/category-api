a5d96f52740eda6f1ebf3a56face805ab191604e
Anjana Nair <anjuviswan93@gmail.com>
2023-02-13 17:51:18 +0530

    flyway configurations fixed

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into category (category_id, category_name, category_description) values (001, 'Appliances', 'A device or piece of equipment designed to perform a specific task, typically a domestic one.');
-insert into item (item_id ,item_name, item_category, item_description, item_price) values (119363, 'Samsung 1.6-cu ft Over-the-Range Microwave', 001 , 'Attractive stainless steel exterior complements modern kitchen decor', 199.00);
--- src/main/resources/db/migration/V1_1__insert_values_into_category_table.sql
+++ src/main/resources/db/migration/V1_1__insert_values_into_category_table.sql
@@ -1,1 +1,1 @@
---insert into category (category_id, category_name, category_description) values (001, 'Appliances', 'A device or piece of equipment designed to perform a specific task, typically a domestic one.');
+insert into category (category_id, category_name, category_description) values (001, 'Appliances', 'A device or piece of equipment designed to perform a specific task, typically a domestic one.');
